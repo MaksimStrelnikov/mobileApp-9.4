6378b45534cc93f35a8a10a54cdf4f0ca1547052
Alex-Sing-bit <dexp51477@gmail.com>
2025-05-23 19:33:41 +0300

    refactor[FRNT-38]: delete unused duplicates

--- app/src/main/java/dev/tp_94/mobileapp/order_payment/domain/CardRepository.kt
+++
@@ -1,9 +0,0 @@
-package dev.tp_94.mobileapp.order_payment.domain
-
-import dev.tp_94.mobileapp.core.models.Card
-import dev.tp_94.mobileapp.core.models.User
-
-interface CardRepository {
-    suspend fun addNewCard(number: String, expiration: String, cvc: String, user: User): Card
-    suspend fun getAllCards(user: User): List<Card>
-}
