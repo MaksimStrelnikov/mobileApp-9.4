c48749cca7d4f4751cf0a8ea53dfeec0a893ae09
Maxim <uservice825@gmail.com>
2025-05-26 13:24:51 +0300

    fix: fix preview render; need to do smth about no image when awaiting for it to load

--- app/src/main/java/dev/tp_94/mobileapp/cakes_feed/presentation/components/CakeFeedItem.kt
+++ app/src/main/java/dev/tp_94/mobileapp/cakes_feed/presentation/components/CakeFeedItem.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
