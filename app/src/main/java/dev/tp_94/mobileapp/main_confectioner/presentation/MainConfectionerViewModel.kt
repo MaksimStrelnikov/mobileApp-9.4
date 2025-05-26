1a55bf8423a660ac334f0039536a6d20e8ad9002
Alex-Sing-bit <dexp51477@gmail.com>
2025-05-26 16:50:41 +0300

    fix[FRNT-38]: fix cake list display at confectioner main page

--- app/src/main/java/dev/tp_94/mobileapp/main_confectioner/presentation/MainConfectionerViewModel.kt
+++ app/src/main/java/dev/tp_94/mobileapp/main_confectioner/presentation/MainConfectionerViewModel.kt
@@ -1,16 +1,24 @@
 package dev.tp_94.mobileapp.main_confectioner.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.tp_94.mobileapp.confectioner_page.domain.GetProductsUseCase
+import dev.tp_94.mobileapp.confectioner_page.presentation.GetProductsResult
 import dev.tp_94.mobileapp.core.SessionCache
 import dev.tp_94.mobileapp.core.models.Confectioner
 import dev.tp_94.mobileapp.core.models.User
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+import java.net.URLDecoder
 import javax.inject.Inject
 
 @HiltViewModel
-class MainConfectionerViewModel @Inject constructor(private val sessionCache: SessionCache) :
+class MainConfectionerViewModel @Inject constructor(
+    private val sessionCache: SessionCache,
+    private val getProductsUseCase: GetProductsUseCase,) :
     ViewModel() {
     fun getUser(): User? {
         return sessionCache.session?.user
@@ -20,13 +28,27 @@ class MainConfectionerViewModel @Inject constructor(private val sessionCache: Se
         sessionCache.clearSession()
     }
 
+    private val confectioner = getUser()!! as Confectioner
+
     private val _state = MutableStateFlow(
         MainConfectionerState(
-            getUser()!! as Confectioner
+            confectioner = confectioner,
+            products = emptyList()
         )
     )
+    init {
+        getProducts()
+    }
 
-    //TODO: add product get
+    private fun getProducts() {
+        viewModelScope.launch {
+            val result = getProductsUseCase.execute(confectioner)
+            if (result is GetProductsResult.Success) {
+                _state.value = _state.value.copy(products = result.cakes)
+            }
+            //TODO: add error handling
+        }
+    }
 
     val state = _state.asStateFlow()
 }
