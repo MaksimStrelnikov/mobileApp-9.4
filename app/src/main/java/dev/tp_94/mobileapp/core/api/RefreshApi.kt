83ac284465abdec6bf62efb9103f35701b8b21e0
Maxim <uservice825@gmail.com>
2025-05-28 12:26:31 +0300

    fix(FRNT-21): fix account deletion

--- app/src/main/java/dev/tp_94/mobileapp/core/api/RefreshApi.kt
+++ app/src/main/java/dev/tp_94/mobileapp/core/api/RefreshApi.kt
@@ -2,10 +2,10 @@ package dev.tp_94.mobileapp.core.api
 
 import dev.tp_94.mobileapp.core.api.dto.RefreshResponseDTO
 import retrofit2.Response
-import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface RefreshApi {
-    @GET("auth/refresh/")
+    @POST("auth/refresh/")
     suspend fun refreshToken(@Header("Authorization") refreshToken: String): Response<RefreshResponseDTO>
 }
--- app/src/main/java/dev/tp_94/mobileapp/profile/domain/DeleteAccountUseCase.kt
+++ app/src/main/java/dev/tp_94/mobileapp/profile/domain/DeleteAccountUseCase.kt
@@ -7,11 +7,13 @@ import dev.tp_94.mobileapp.profile.presentation.DeleteAccountResult
 import javax.inject.Inject
 
 class DeleteAccountUseCase @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val sessionCache: SessionCache
 ) {
     suspend fun execute(): DeleteAccountResult {
         try {
             userRepository.delete()
+            sessionCache.clearSession()
             return DeleteAccountResult.Success
         } catch (e: Exception) {
             return DeleteAccountResult.Error(e.message ?: "Возникла непредвиденная ошибка")
