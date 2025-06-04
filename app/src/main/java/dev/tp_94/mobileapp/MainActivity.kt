7c0ba41fb2172a2455c0c441f794f56296d0a139
Alex-Sing-bit <dexp51477@gmail.com>
2025-06-04 11:39:06 +0300

    fix[FRNT-40]: fix actionBar visibility

--- app/src/main/java/dev/tp_94/mobileapp/MainActivity.kt
+++ app/src/main/java/dev/tp_94/mobileapp/MainActivity.kt
@@ -103,6 +103,8 @@ class MainActivity : ComponentActivity() {
             !isAppInitialized.value
         }
 
+        actionBar?.hide()
+
         super.onCreate(savedInstanceState)
 
         setContent {
